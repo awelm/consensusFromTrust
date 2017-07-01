33b532f26e397ab51504a399d02ba43024ad920e
Akila Welihinda <akilawelihinda@ucla.edu>
2017-06-30 18:49:56 -0700

    very simple algorithm

--- CompliantNode.java
+++ CompliantNode.java
@@ -11,11 +11,8 @@ public class CompliantNode implements Node {
     private boolean[] followees;
     private int trustCount;
     private Set<Transaction> proposals;
-    private Set<Transaction> consensus;
-    private boolean proposalStage;
 
     public CompliantNode(double p_graph, double p_malicious, double p_txDistribution, int numRounds) {
-        proposalStage = true;
         trustCount = 0;
     }
 
@@ -30,43 +27,19 @@ public class CompliantNode implements Node {
         this.proposals = new HashSet<Transaction>(pendingTransactions);
     }
 
-    /* The first time this method is called, it returns its proposals that it
-     * will send to its followers. The second time its called, it returns the
+    /* Each call to this method (except the last call) returns the node's proposals that it
+     * will send to its followers. During the last call, it returns the
      * transactions that it belives are the consensus.
     */
     public Set<Transaction> sendToFollowers() {
-        if(proposalStage) { // return proposal list
-            proposalStage = !proposalStage;
-            return proposals;
-        }
-        else { // return consensus list
-            proposalStage = !proposalStage;
-            return consensus;
-        }
+        return proposals;
     }
 
     public void receiveFromFollowees(Set<Candidate> candidates) {
-        HashMap<Transaction, Integer> votes = new HashMap<Transaction, Integer>();
-        consensus = new HashSet<Transaction>();
-
-        //include your own proposals when determining consensus
-        for(Transaction t: proposals)
-            votes.put(t,1);
-
-        // save trusted followees votes
-        for(Candidate c: candidates)
-        {
-            if(followees[c.sender]) {
-                int count = 0;
-                if (votes.get(c.tx) != null)
-                    count = votes.get(c.tx);
-                votes.put(c.tx, count+1);
-            }
+        for(Candidate c: candidates) {
+            Transaction tx = c.tx;
+            if(followees[c.sender])
+                proposals.add(c.tx);
         }
-
-        for (Map.Entry<Transaction, Integer> entry : votes.entrySet()) {
-            if(entry.getValue() >= 0) //TODO: temp, remove this
-                consensus.add(entry.getKey());
-        } 
     }
 }
